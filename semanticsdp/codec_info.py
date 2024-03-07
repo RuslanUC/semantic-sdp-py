7f662f54645ad156bbfaa42ae473c09a33ee598e
RuslanUC <dev_ruslan_uc@protonmail.com>
2024-03-07 17:23:50 +0200

    lower minimal python version to 3.9

--- semanticsdp/codec_info.py
+++ semanticsdp/codec_info.py
@@ -1,6 +1,6 @@
 from __future__ import annotations
 
-from dataclasses import dataclass, asdict, field
+from dataclasses import dataclass, field
 
 from semanticsdp import RTCPFeedbackInfo, BaseSdp
 
